dc94a2d887eece6a40e83ca41708255c4d4e5c59
Julia Kaczmarek <ylaokk@outlook.com>
2020-11-23 14:28:44 +0100

    ile do końca praktyk

---
+++ Praksy/Apka.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Praksy
+{
+    partial class Apka
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.aktualnaData = new System.Windows.Forms.Label();
+            this.aktualnyCzas = new System.Windows.Forms.Label();
+            this.doKoncaPraktyk = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // aktualnaData
+            // 
+            this.aktualnaData.AutoSize = true;
+            this.aktualnaData.Location = new System.Drawing.Point(12, 23);
+            this.aktualnaData.Name = "aktualnaData";
+            this.aktualnaData.Size = new System.Drawing.Size(76, 13);
+            this.aktualnaData.TabIndex = 0;
+            this.aktualnaData.Text = "Aktualna data:";
+            // 
+            // aktualnyCzas
+            // 
+            this.aktualnyCzas.AutoSize = true;
+            this.aktualnyCzas.Location = new System.Drawing.Point(12, 47);
+            this.aktualnyCzas.Name = "aktualnyCzas";
+            this.aktualnyCzas.Size = new System.Drawing.Size(76, 13);
+            this.aktualnyCzas.TabIndex = 1;
+            this.aktualnyCzas.Text = "Aktualny czas:";
+            // 
+            // doKoncaPraktyk
+            // 
+            this.doKoncaPraktyk.AutoSize = true;
+            this.doKoncaPraktyk.Location = new System.Drawing.Point(12, 96);
+            this.doKoncaPraktyk.Name = "doKoncaPraktyk";
+            this.doKoncaPraktyk.Size = new System.Drawing.Size(95, 13);
+            this.doKoncaPraktyk.TabIndex = 2;
+            this.doKoncaPraktyk.Text = "Do końca praktyk:";
+            // 
+            // Apka
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 137);
+            this.Controls.Add(this.doKoncaPraktyk);
+            this.Controls.Add(this.aktualnyCzas);
+            this.Controls.Add(this.aktualnaData);
+            this.Name = "Apka";
+            this.Text = "Apka";
+            this.Load += new System.EventHandler(this.Apka_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label aktualnaData;
+        private System.Windows.Forms.Label aktualnyCzas;
+        private System.Windows.Forms.Label doKoncaPraktyk;
+    }
+}
+
---
+++ Praksy/Apka.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Praksy
+{
+    public partial class Apka : Form
+    {
+        Timer t = new Timer();
+        DateTime koniecPraktyk = new DateTime(2020, 12, 11,15,0,0);
+        //rrrr,mm,dd,hh,mm,ss
+        public Apka()
+        {
+            InitializeComponent();
+
+        }
+        private void Apka_Load(object sender, EventArgs e)
+        {
+            //co sekunde
+            t.Interval = 1000;
+            t.Tick += new EventHandler(this.t_Tick);
+            t.Start();
+        }
+        private void t_Tick(object sender, EventArgs e)
+        {
+            //aktualny czas
+            aktualnyCzas.Text = "Aktualny czas: " + DateTime.Now.ToLongTimeString();
+            aktualnaData.Text = "Aktualna data: " + DateTime.Now.ToLongDateString(); 
+            //czas do końca praktyk 
+            doKoncaPraktyk.Text = "Do końca praktyk: " + koniecPraktyk.Subtract(DateTime.Now).Days +" dni " + koniecPraktyk.Subtract(DateTime.Now).Hours + " godzin " + koniecPraktyk.Subtract(DateTime.Now).Minutes + " minut "+ koniecPraktyk.Subtract(DateTime.Now).Seconds + " sekund.";
+        }
+    }
+}
